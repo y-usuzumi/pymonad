0f4ce3745bfae9a5599e2d30e46b09dd998c1d13
Jason DeLaat <jason.develops@gmail.com>
2014-06-15 10:59:49 -0400

    Added LazyReader.py to the import list in __init__.py

--- pymonad/__init__.py
+++ pymonad/__init__.py
@@ -7,6 +7,7 @@ from pymonad.Functor import *
 from pymonad.Applicative import *
 from pymonad.Monad import *
 from pymonad.Reader import *
+from pymonad.LazyReader import *
 from pymonad.Maybe import *
 from pymonad.Either import *
 from pymonad.List import *
